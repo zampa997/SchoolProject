b07d722176abea0eb11b6489bae6f22d6fb09492
samuel rizzi <samuelrizzi.sr@gmail.com>
2021-09-30 17:31:03 +0200

    30/09 17:30 SAM

---
+++ ef_scaffold/Repository/SpecificRepository/CorsoRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataLayer.Repositories;
+using ef_scaffold.EfData;
+using ef_scaffold.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ef_scaffold.Repository.SpecificRepository
+{
+    public class CorsoRepository : CrudRepository<Corso, long>, ICorsoRepository
+    {
+        public CorsoRepository(SchoolContext ctx) : base(ctx)
+        {
+
+        }
+    }
+}
